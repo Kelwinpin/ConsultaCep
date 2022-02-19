fbe31a4c98e26fb94dc23b04955119a32254798e
Kelwinpin <kelwincomw@gmail.com>
2022-02-19 15:24:15 -0300

    Trocando trechos do javascript pelo Jquery

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function consultar(){
-   var cep = document.getElementById("Cep").value;
+   var cep = document.getElementById("CEP").value;
    var url = "https://viacep.com.br/ws/"+ cep +"/json/";
 
    console.log(url);
@@ -8,10 +8,15 @@ function consultar(){
       url: url,
       type:"GET",
       success: function(response){
-         document.getElementById("UF").innerHTML = response.uf;
-         document.getElementById("Bairro").innerHTML = response.bairro;
-         document.getElementById("Rua").innerHTML = response.logradouro;
+            $("#uf").html(response.uf);
+            $("#bairro").html(response.bairro);
+            $("#logradouro").html(response.logradouro);
+            $("#localidade").html(response.localidade);
+            $("#cep").show();
       }
    })
+}   
 
-}
+$(function(){
+      $("#cep").hide();
+   })
